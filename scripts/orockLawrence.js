9888f4f496e9696e81cfda4ec92cf98dfdf6f587
Bessong Lawrence <orockbessonglawrence@gmail.com>
2020-06-04 16:03:16 +0100

    Recloned and completed the stage 2 task again

---
+++ scripts/orockLawrence.js
@@ -0,0 +1,5 @@
+const Task = (name, id, email, stack) => {
+	console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${stack} for stage 2 task`);
+}
+
+Task("OROCK LAWRENCE", "HNG-01327", "orockbessonglawrence@gmail.com", "javascript");
